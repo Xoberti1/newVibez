5cfd950557cd69635d43b3cfd2f896ecc331ca14
Isla <hatfieldisla@gmail.com>
2018-03-21 18:14:04 -0700

    chat styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Signin from "./pages/signin/Signin";
 import Shows from "./pages/concerts/Shows";
-// import Profile from "./pages/Profile/Profile"
+import Profile from "./pages/Profile/Profile"
 import { Button } from 'reactstrap';
 import ChatApp from "./pages/chatroom/App";
-// import register from "./pages/register";
-// import Signin from "./pages/signin/Signin";
-// import shows from "./pages/concerts/shows";
-// import chatroom from "./pages/chatroom";
 import register from "./pages/registration/registration";
 // import Navbar from "./components/Navbar";
 // import Wrapper from "./components/Wrapper";
@@ -17,13 +13,13 @@ import register from "./pages/registration/registration";
 const App = () =>
   <Router>
     <div>
-      {/* <Navbar /> */}
+       {/* <Navbar />  */}
         <Route exact path="/register" component={register} />
         <Route exact path="/" component={Signin} />
-        {/* <Route exact path="/Profile" component={Profile} /> */}
+       <Route exact path="/Profile" component={Profile} /> */}
         <Route exact path="/Shows" component={Shows} />
         <Route exact path="/chatroom" component={ChatApp} />
-       {/* <Footer /> */}
+       {/* <Footer />  */}
     </div>
   </Router>;
 
